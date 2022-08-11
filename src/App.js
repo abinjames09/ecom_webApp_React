12b5d318bdb7a0312df1b48ec65964a2d4feab77
Abin James <abinjames409@gmail.com>
2022-08-11 13:06:25 +0530

    firebase connected

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React from 'react';
+import {BrowserRouter as Router,Route} from 'react-router-dom'
 import './App.css';
+import Signup from './Pages/Signup'
 
 /**
  * ?  =====Import Components=====
@@ -9,7 +11,14 @@ import Home from './Pages/Home';
 function App() {
   return (
     <div>
+      <Router>
+        <Route exact path='/'>
       <Home />
+      </Route>
+      <Route path='/signup'>
+      <Signup />
+      </Route>
+      </Router>
     </div>
   );
 }
---
+++ src/firebase/config.js
@@ -0,0 +1,11 @@
+import firebase from "firebase";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCJgbA18GkDoccybsXgIPkpYpuylPHI9Zs",
+    authDomain: "olx-clone-abbf9.firebaseapp.com",
+    projectId: "olx-clone-abbf9",
+    storageBucket: "olx-clone-abbf9.appspot.com",
+    messagingSenderId: "720186634909",
+    appId: "1:720186634909:web:05998d14b716b00afa5a74"
+  };
+  export default firebase.initializeApp(firebaseConfig)
